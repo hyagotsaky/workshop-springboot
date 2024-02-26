12f341cc7a58d846c35fbc699339d4452f98a1ed
C1326348 Hyago Souza <C1326348@interno.bb.com.br>
2024-02-26 10:07:14 -0300

    subTotal e Total

--- src/main/java/com/Hdev/course/entities/Order.java
+++ src/main/java/com/Hdev/course/entities/Order.java
@@ -10,6 +10,7 @@ import com.Hdev.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -17,6 +18,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -42,6 +44,10 @@ public class Order implements Serializable {
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 
+	
+	@OneToOne(mappedBy = "order" ,cascade = CascadeType.ALL)
+	private Payment payment;
+	
 	public Order() {
 	}
 
@@ -86,8 +92,26 @@ public class Order implements Serializable {
 		this.client = client;
 	}
 	
+	
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
+	}
+
 	public Set<OrderItem> getItems() {
 		return items;
+	}
+	
+	public Double getTotal() {
+		Double soma = 0.0;
+		for (OrderItem x : items) {
+			soma += x.getSubTotal();
+		}
+		return soma;
 	}
 
 	@Override
