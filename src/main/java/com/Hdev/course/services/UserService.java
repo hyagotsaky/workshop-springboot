0223aa91d6161f2b0a6b448f703d374b4a44bc6d
C1326348 Hyago Souza <C1326348@interno.bb.com.br>
2024-02-27 17:16:48 -0300

    excecao para update

--- src/main/java/com/Hdev/course/services/UserService.java
+++ src/main/java/com/Hdev/course/services/UserService.java
@@ -4,11 +4,14 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.Hdev.course.entities.User;
 import com.Hdev.course.repositories.UserRepository;
 import com.Hdev.course.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
 
 @Service
 public class UserService {
@@ -30,13 +33,23 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);			
+		}catch (RuntimeException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity,obj);
-		return repository.save(entity);
+		
+		try {
+			
+			User entity = repository.getReferenceById(id);
+			updateData(entity,obj);
+			return repository.save(entity);
+		}catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
